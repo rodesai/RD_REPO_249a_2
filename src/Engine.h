a4aaf2b5a713ee21d7c662b22c4e5fec6d26bb5b
Rohan Desai <desai.p.rohan@gmail.com>
2012-10-21 16:36:25 -0700

    3 Changes: 1) Added Notifiee classes for Segment and ShippingEngine, 2) Added segment reactor that handles all Segment management (updates old and new source Locations, old and new return Segments), 3) Changed ShippingEngine to contain factory methods for all Entity types so that we can guarantee all invariants are maintained (each segment gets added to the map,has its reactors set up, etc)

--- src/Engine.h
+++ src/Engine.h
@@ -27,6 +27,7 @@ class ArgumentException : public std::exception {
     }
 };
 
+// Primitive Types
 
 class Mile : public Ordinal<Mile, uint64_t> {
 public:
@@ -75,9 +76,13 @@ public:
     PackageNum(uint64_t num) : Ordinal<PackageNum, uint64_t>(num) {}
 };
 
+// Core Types
+
 class Segment : public Fwk::PtrInterface<Segment>{
 
 public:
+
+    // Class Types
 
     typedef Fwk::Ptr<Segment> Ptr;
     typedef Fwk::Ptr<Segment const> PtrConst;
@@ -91,6 +96,28 @@ public:
         truckSegment_ = 0,
         boatSegment_ = 1,
         planeSegment_ = 2
+    };
+
+    // Notifiees
+
+    class Notifiee : public Fwk::PtrInterface<Segment::Notifiee>{
+
+    public:
+
+        typedef Fwk::Ptr<Segment::Notifiee> Ptr;
+        typedef Fwk::Ptr<Segment::Notifiee const> PtrConst;
+
+        // Events
+        virtual void onSource(){}
+        virtual void onReverseSegment(){}
+
+        void notifierIs(Segment::Ptr notifier){ notifier_=notifier; }
+        Segment::Ptr notifier() const { return notifier_; }
+
+    protected:
+        Notifiee(){}
+    private:
+        Segment::Ptr notifier_;
     };
 
     // accesors
@@ -107,38 +134,36 @@ public:
     inline Difficulty difficulty() const { return difficulty_; }
     inline ExpediteSupport expediteSupport() const { return expediteSupport_; }
 
+    Segment::Notifiee::Ptr notifiee(){ return notifiee_; }
+
     // mutators
-    void sourceIs(LocationID source){
-        // remove segment from previous source, if exists
-        // set source
-        // add segment to new source
-    }
+    void sourceIs(LocationID source);
     void lengthIs(Mile l) { length_ = l; }
     void returnSegmentIs(Segment::Ptr s) { returnSegment_ = s; }
     void difficultyIs(Difficulty d) { difficulty_ = d; }
     void expediteSupportIs(ExpediteSupport es) { expediteSupport_ = es; }
 
-    Segment::Ptr SegmentIs(SegmentID id, EntityType type){
-        return new Segment(id, type);
+    void notifieeIs(Segment::Notifiee::Ptr notifiee){ 
+        notifiee->notifierIs(this);
+        notifiee_ = notifiee; 
     }
 
 private:
 
+    friend class ShippingEngine;
+
     Segment(SegmentID id, EntityType type) : length_(0), difficulty_(1.0), expediteSupport_(no_), id_(id), entityType_(type){}
 
+    // attributes
     Mile length_;
-
     Difficulty difficulty_;
-
     ExpediteSupport expediteSupport_;
-
     SegmentID id_;
-
     EntityType entityType_;
-
     Segment::Ptr returnSegment_;
-
     LocationID source_;
+
+    Segment::Notifiee::Ptr notifiee_;
 };
 
 
@@ -202,10 +227,9 @@ public:
     typedef Fwk::Ptr<Customer> Ptr;
     typedef Fwk::Ptr<Customer const> PtrConst;
 
-    // Instantiator
-    Customer::Ptr CustomerIs();
-
 private:
+
+    friend class ShippingEngine;
 
     // Constructor
     Customer() : Location(Location::customer()){}
@@ -219,10 +243,9 @@ public:
     typedef Fwk::Ptr<Port> Ptr;
     typedef Fwk::Ptr<Port const> PtrConst;
 
-    // Instantiator
-    Port::Ptr PortIs();
-
 private:
+
+    friend class ShippingEngine;
 
     // Constructor
     Port() : Location(Location::port()){}
@@ -261,6 +284,9 @@ public:
     TruckTerminal::Ptr TruckTerminalIs();
 
 private:
+
+    friend class ShippingEngine;
+
     // Constructor
     TruckTerminal() : Terminal(Location::truckTerminal(),Segment::truckSegment()){}
 };
@@ -273,10 +299,10 @@ public:
     typedef Fwk::Ptr<BoatTerminal> Ptr;
     typedef Fwk::Ptr<BoatTerminal const> PtrConst;
 
-    // Instantiator
-    BoatTerminal::Ptr BoatTerminalIs();
-
 private:
+
+    friend class ShippingEngine;
+
     // Constructor
     BoatTerminal() : Terminal(Location::boatTerminal(),Segment::boatSegment()){}
 };
@@ -289,10 +315,9 @@ public:
     typedef Fwk::Ptr<PlaneTerminal> Ptr;
     typedef Fwk::Ptr<PlaneTerminal const> PtrConst;
 
-    // Instantiator
-    PlaneTerminal::Ptr PlaneTerminalIs();
-
 private:
+
+    friend class ShippingEngine;
 
     // Constructor
     PlaneTerminal() : Terminal(planeTerminal_,Segment::planeSegment()){}
@@ -408,10 +433,46 @@ class ShippingEngine : public Fwk::PtrInterface<Stats>{
 
 public:
 
+    // class types
+
     typedef Fwk::Ptr<ShippingEngine> Ptr;
     typedef Fwk::Ptr<ShippingEngine const> PtrConst;
 
+    // notifiee
+
+    class Notifiee : public Fwk::PtrInterface<ShippingEngine::Notifiee>{
+    public:
+
+        typedef Fwk::Ptr<ShippingEngine::Notifiee> Ptr;
+        typedef Fwk::Ptr<ShippingEngine::Notifiee const> PtrConst;
+
+        // Segments
+        virtual void onSegmentNew(SegmentID segmentID){}
+        virtual void onSegmentDel(Segment::Ptr segment){}
+
+        // Locations
+        virtual void onCustomerNew(LocationID locationID){}
+        virtual void onCustomerDel(Customer::Ptr customer){}
+        virtual void onPortNew(LocationID locationID){}
+        virtual void onPortDel(Port::Ptr port){}
+        virtual void onPlaneTerminalNew(LocationID locationID){}
+        virtual void onPlaneTerminalDel(PlaneTerminal::Ptr planeTerminal){}
+        virtual void onBoatTerminalNew(LocationID locationID){}
+        virtual void onBoatTerminalDel(BoatTerminal::Ptr boatTerminal){}
+        virtual void onTruckTerminalNew(LocationID locationID){}
+        virtual void onTruckTerminalDel(TruckTerminal::Ptr truckTerminal){}
+ 
+    protected:
+
+        Notifiee() {}
+
+    private:
+
+        ShippingEngine::Ptr shippingEngine_;
+    };
+
     // accessor
+
     Segment::Ptr segement(SegmentID sid) { return segmentMap_[sid]; }
     Location::Ptr location(LocationID lid) { return locationMap_[lid]; }
     Conn::Ptr conn(ConnID cid) const;
@@ -419,21 +480,91 @@ public:
     Fleet::Ptr fleet(FleetID fid) const;
 
     // mutators
-    void segmentIs(Segment::Ptr s, SegmentID name) { segmentMap_[name] = s; }
-    void segmentDel();
-    void locationIs(Location::Ptr s, LocationID name) { locationMap_[name] = s; }
-    void locationDel();
+
+    // Instance Creators
+    // These instance creators create an instance, add it to the map, 
+    // and set up any needed reactors;
+
+    Segment::Ptr SegmentNew(SegmentID name, Segment::EntityType entityType); 
+    Segment::Ptr segmentDel();
+
+    Location::Ptr CustomerNew(LocationID name);
+    Location::Ptr customerDel();
+
+    Location::Ptr PortNew(LocationID name);
+    Location::Ptr portDel();
+
+    Location::Ptr TruckTerminalNew(LocationID name);
+    Location::Ptr truckTerminalDel();
+
+    Location::Ptr BoatTerminalNew(LocationID name);
+    Location::Ptr boatTerminalDel();
+
+    Location::Ptr PlaneTerminalNew(LocationID name);
+    Location::Ptr planeTerminalDel();
+
+    // These instance creators should only create instance on first call
+
+    Conn::Ptr ConnNew(ConnID name);
+    Conn::Ptr connDel(ConnID name);
+
+    Stats::Ptr StatsNew(StatsID name);
+    Stats::Ptr statsDel(StatsID name);
+
+    Fleet::Ptr FleetNew(FleetID name);
+    Fleet::Ptr fleetDel(FleetID name);
+
+    void notifieeIs(Segment::Notifiee::Ptr notifiee);
 
 private:
 
+    // attributes
     typedef std::map<LocationID, Location::Ptr> LocationMap;
     LocationMap locationMap_;
-
     typedef std::map<SegmentID, Segment::Ptr> SegmentMap;
     SegmentMap segmentMap_;
+
+    // notifiees
+    typedef std::vector<ShippingEngine::Notifiee::Ptr> NotifieeList;
+    NotifieeList notifieees_;
 };
 
-class ShippingEngineReactor : public Fwk::PtrInterface<ShippingEngineReactor>{
+class SegmentReactor : public Segment::Notifiee{
+
+public:
+
+    typedef Fwk::Ptr<SegmentReactor> Ptr;
+    typedef Fwk::Ptr<SegmentReactor const> PtrConst;
+
+    /* Remove owner from the current source
+     * Add owner to the new source
+     * Reset current source to the new source
+     */
+    void onSource();
+
+    /* Remove owner from current reverse segment
+     * Add owner to the new reverse segment
+     * Reset the current reverse segment to the 
+     *   new reverse segment
+     */ 
+    void onReverseSegment();  
+
+    static SegmentReactor::Ptr SegmentReactorIs(){
+        return new SegmentReactor();
+    }
+
+private:
+
+    SegmentReactor(){
+        currentSource_=NULL;
+        currentReverse_=NULL;
+    }
+
+    Location::Ptr currentSource_;
+    Segment::Ptr  currentReverse_;
+};
+
+class ShippingEngineReactor : public ShippingEngine::Notifiee{
 
 public:
 
@@ -445,37 +576,43 @@ public:
         // remove segment from reverse segment
     }
 
+    static ShippingEngineReactor::Ptr ShippingEngineReactorIs(){
+        return new ShippingEngineReactor();
+    }
+
 private:
-    ShippingEngine::Ptr owner_;
+
+    ShippingEngineReactor(){}
 };
 
 
-class StatsReactor : public Fwk::PtrInterface<Stats>{
+class StatsReactor : public ShippingEngine::Notifiee{
 
 public:
 
     typedef Fwk::Ptr<StatsReactor> Ptr;
     typedef Fwk::Ptr<StatsReactor const> PtrConst;
 
-    void onSegmentNew(SegmentID segment) {
-        // increment counts
-    }
+    void onSegmentNew(SegmentID segmentID);
+    void onSegmentDel(Segment::Ptr segment);
+    void onCustomerNew(LocationID locationID);
+    void onCustomerDel(Customer::Ptr customer);
+    void onPortNew(LocationID locationID);
+    void onPortDel(Port::Ptr port);
+    void onPlaneTerminalNew(LocationID locationID);
+    void onPlaneTerminalDel(PlaneTerminal::Ptr planeTerminal);
+    void onBoatTerminalNew(LocationID locationID);
+    void onBoatTerminalDel(BoatTerminal::Ptr boatTerminal);
+    void onTruckTerminalNew(LocationID locationID);
+    void onTruckTerminalDel(TruckTerminal::Ptr truckTerminal);
 
-    void onSegmentDel(Segment::Ptr segment) {
-        // decrement counts
-    }
-
-    void onLocationNew(LocationID location) {
-        // increment counts
-    }
-
-    void onLocationDel(Location::Ptr location) {
-        // decrement counts
+    static StatsReactor::Ptr StatsReactorIs(){
+        return new StatsReactor();
     }
 
 private:
 
-    Stats::Ptr owner_;
+    StatsReactor(){}
 };
 
 } /* end namespace */
